2f5830886bf33a3d83c1b56f2361667864452821
Gitlab CI Network <network@sportradar.com>
2023-10-12 20:19:54 +0200

    VirtualStadiumDataSDK 1.1.415 release

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     .library(name: "VirtualStadiumDataSDK", targets: [ "VirtualStadiumDataSDK" ]),
   ],
   targets: [
-    .binaryTarget(name: "VirtualStadiumDataSDK", url: "https://artifacts.vs.sportradar.com/virtualstadium/datasdk/apple/libs/1.1.400/VirtualStadiumDataSDK.xcframework.zip", checksum: "0bef5e63ae1cf2efa311790f0287e5a85b244872330ed33886d595eea0b42f49"),
+    .binaryTarget(name: "VirtualStadiumDataSDK", url: "https://artifacts.vs.sportradar.com/virtualstadium/datasdk/apple/libs/1.1.415/VirtualStadiumDataSDK.xcframework.zip", checksum: "93f4a866d84bdd3032ee7f5fc92249046a78d852b482314634f7e840c55276b7"),
   ]
 )
