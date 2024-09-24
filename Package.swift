1134158c459860f956bbbb8958d184f63b5908fe
buildserver2 <buildserver2@lju1-buildserver2.si.sportradar.ag>
2024-09-24 13:05:05 +0200

    VirtualStadiumDataSDK 1.2.1190 release

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
     .library(name: "VirtualStadiumDataSDK", targets: [ "VirtualStadiumDataSDK" ]),
   ],
   targets: [
-    .binaryTarget(name: "VirtualStadiumDataSDK", url: "https://artifacts.vs.sportradar.com/virtualstadium/datasdk/apple/libs/1.2.1177/VirtualStadiumDataSDK.xcframework.zip", checksum: "463c34d4593784c7491c7be2fdf105e587c4bef6a4f81f45f5fa7f1306315a1a"),
+    .binaryTarget(name: "VirtualStadiumDataSDK", url: "https://artifacts.vs.sportradar.com/virtualstadium/datasdk/apple/libs/1.2.1190/VirtualStadiumDataSDK.xcframework.zip", checksum: "539c8e4d8c47513f95338fb825d24e3d6126bc70f899cc05c61240140778781b"),
   ]
 )
