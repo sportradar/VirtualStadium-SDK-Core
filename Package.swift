166c912689d0e49ae292dc4138e061ec66475912
CI Automation <ci@sportradar.ag>
2026-06-23 15:49:25 +0000

    Update Package.swift for 1.2.5146

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
     .library(name: "VirtualStadiumDataSDK", targets: [ "VirtualStadiumDataSDK" ]),
   ],
   targets: [
-    .binaryTarget(name: "VirtualStadiumDataSDK", url: "https://github.com/sportradar/VirtualStadium-SDK-Core/releases/download/1.2.5144/VirtualStadiumDataSDK.xcframework.zip", checksum: "9d7941522cd2db17965cf5d5e7ff7dd3d8e66c17129e8f9516bb7d2e8fea03d0"),
+    .binaryTarget(name: "VirtualStadiumDataSDK", url: "https://github.com/sportradar/VirtualStadium-SDK-Core/releases/download/1.2.5146/VirtualStadiumDataSDK.xcframework.zip", checksum: "34e795b0156299e17c81649ab6e462c516c2e0f57fa48527e0fb8b16c17810bc"),
   ]
 )
